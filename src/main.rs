b0278c67e980759af70c6af22b2a7a1941f36923
Josun <dennistai.mail@gmail.com>
2022-07-04 21:26:25 +0800

    bruh

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::OpenOptions, io::Write};
+use std::{fs::{File}, io::{Write, stdin}};
 
 use image::GenericImageView;
 
@@ -6,12 +6,22 @@ static ASCII_LUMINANCE_STR: &str =
     "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`\'. ";
 
 fn main() {
-    let img = image::open("kirby.png").unwrap();
-    let (height, width) = img.dimensions();
-    println!("{} {}", height, width);
-    let mut canvas = vec![vec!['#'; height as usize]; width as usize];
-    let mut f = OpenOptions::new().write(true).open("test.txt").unwrap();
+    println!("Image path : ");
+    //get input first
+    let mut s= String::new();
+    stdin().read_line(&mut s).expect("Please enter a file name");
 
+    //remove EoL characters
+    s = s.replace("\n", "").replace("\r", "");
+
+    //init image
+    let img = image::open(&s).unwrap();
+    let (height, width) = img.dimensions();
+    let mut canvas = vec![vec!['#'; height as usize]; width as usize];
+    std::fs::remove_file("output.txt").expect("File delete failed");
+    let mut f = File::create("output.txt").unwrap();    
+
+    //convert image to 2d array of geryscale
     for i in 0..width - 1 {
         for j in 0..height - 1 {
             let pixel = img.get_pixel(j, i);
@@ -20,6 +30,7 @@ fn main() {
             let g = pixel[1] as f64;
             let b = pixel[2] as f64;
 
+            //don't ask why is this forumla, someone figuredd this out
             let mut lum = 0.299 * r + 0.587 * g + 0.114 * b;
             lum = (lum / 3.65).floor();
             let index = lum as usize;
@@ -28,6 +39,7 @@ fn main() {
         }
     }
 
+    //write file
     for i in 0..width - 1 {
         let row = &canvas[i as usize];
         let mut s: String = row.into_iter().collect();
